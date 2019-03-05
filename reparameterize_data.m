f52652dc6547bac2f9a8af8681af87cbfbd799dd
CarolineHolmes <cholmes@princeton.edu>
2019-03-05 17:56:32 -0500

    Updated to allow higher dimensional inputs

--- reparameterize_data.m
+++ reparameterize_data.m
@@ -1,19 +1,4 @@
-function [transformedValues] = reparameterize_data(listOfValues)
-
-% function [transformedValues] = reparameterize_data(listOfValues)
-%
-% This function reparamaterizes the data to a standard normal distribution. 
-%
-% listOfValues -- one dimensional real valued array of data. For example, 
-% it could be one of the components of the multidimensional X variable 
-% in our information estimation. This reparamaterization, if being
-% used, should be performed on each dimension independently.
-%
-% This reparamaterization does not change the ordering of the points -
-% instead, it just changes the distances along the axis.
-%
-%-----------------------------------------------------------------------------------------
-% Copyright 2018, 2019 Caroline Holmes, Ilya Nemenman
+% Copyright 2018 Caroline Holmes, Ilya Nemenman
 %-----------------------------------------------------------------------------------------
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -30,9 +15,7 @@ function [transformedValues] = reparameterize_data(listOfValues)
 %-----------------------------------------------------------------------------------------
 % Please reference
 % 
-% Holmes, C.M. & Nemenman, I.  Estimation of mutual information for
-% real-valued data with error bars and controlled bias. 
-% Submitted, 2019.
+%(OUR PAPER)
 %
 % A. Kraskov, H. Stogbauer, and P. Grassberger,
 % Estimating mutual information.
@@ -41,8 +24,40 @@ function [transformedValues] = reparameterize_data(listOfValues)
 % in your published research.
 
 
-[A,I] = sort(listOfValues); % data solrted by their values and their index
-            % in the original data set
+function [transformedValues] = reparameterize_data(listOfValues)
+
+%This reparamaterizes our data to a gaussian. listOfValues can represent
+%multi-dimensional data, and the reparameterization will be done on each
+%dimension independently. If reparamaterization on only some dimensions is
+%desired, only those dimensions should be input to this function.
+
+%This function assumes that the listOfValues is a matrix, where the larger
+%dimension counts through samples of the distribution, and the smaller
+%dimension counts through dimensions of the data.
+
+%This reparamaterization does not change the ordering of the points -
+%instead, it just changes the distances along the axis so that you have a
+%gaussian distribution. 
+
+% However, in order to remove a potential artifact associated with data
+% points with the same value, we reorder the data randomly before
+% reparamaterizing, and then return it to its original ordering.
+if size(listOfValues,2) > size(listOfValues,1)
+    listOfValues = listOfValues';
+end
+
+
+
+if min(size(listOfValues)) == 1
+
+%reorder the data:
+
+
+ordering = randperm(length(listOfValues));
+newListOfValues = listOfValues(ordering);
+
+
+[A,I] = sort(newListOfValues); 
 
 listPositions = zeros(length(listOfValues),1);
 
@@ -50,9 +65,57 @@ for i = 1:length(listOfValues)
     listPositions(I(i)) = i;
 end
 
-%reassign each i'th value to where the i'th value would have been in a 
-%standard normal dataset using the erfinv function.
+%reassign to be in range where can use erfinv: need to be between -1 and 1.
+
 
 listPositions = listPositions-mean(listPositions);
 listPositions = 2.*listPositions./(length(listPositions));
-transformedValues = sqrt(2)*erfinv(listPositions);
+
+randOrdering_transformedValues = erfinv(listPositions);
+
+transformedValues = zeros(size(listOfValues));
+%return to the original ordering
+for i = 1:length(listOfValues)
+    transformedValues(ordering(i)) = randOrdering_transformedValues(i);
+end
+
+else
+% for the case where we have multiple dimensions. Essentially, this just
+% loops through the above behaviour, operating on each dimension
+% independently.
+    
+x2 = zeros(size(listOfValues));
+input = listOfValues;
+for iiii = 1:min(size(input)) 
+    listOfValues = input(:,iiii);
+    ordering = randperm(length(listOfValues));
+newListOfValues = listOfValues(ordering);
+
+
+[A,I] = sort(newListOfValues); 
+
+listPositions = zeros(length(listOfValues),1);
+
+for i = 1:length(listOfValues)
+    listPositions(I(i)) = i;
+end
+
+%reassign to be in range where can use erfinv: need to be between -1 and 1.
+
+
+listPositions = listPositions-mean(listPositions);
+listPositions = 2.*listPositions./(length(listPositions));
+
+randOrdering_transformedValues = erfinv(listPositions);
+
+transformedValues = zeros(size(listOfValues));
+%return to the original ordering
+for i = 1:length(listOfValues)
+    transformedValues(ordering(i)) = randOrdering_transformedValues(i);
+end
+x2(:,iiii) = transformedValues;
+end
+transformedValues = x2;
+    
+end
+end

7243b6e6f0e63f4eff111e4d0494b359bc52312c
CarolineHolmes <cholmes@princeton.edu>
2019-03-23 18:36:39 -0400

    Updated help text

--- ExampleScripts/finchDataExample.m
+++ ExampleScripts/finchDataExample.m
@@ -1,27 +1,77 @@
-function [] = finchDataExample()
-%Runs through some different analyses of the Bengalese Finch data - comparing k and N
-%dependence of mutual information estimates, and showing the effects of
-%reparameterization.
+function [ks, unreparamaterizedEstimates, unreparamaterizedErrorBars, reparamaterizedEstimates, ...
+    reparamaterizedErrorBars] = finchDataExample()
+%function [ks, unreparamaterizedEstimates, unreparamaterizedErrorBars, reparamaterizedEstimates, ...
+%    reparamaterizedErrorBars] = finchDataExample()
+%
+% Runs through some different analyses of the NF-kB data - comparing k and N
+% dependence of mutual information estimates, and showing the effects of
+% reparameterization. The data set is described in more detail in Ref. [1].
+% Briefly, the data describe the interspike intervals of a motor neuron
+% that controls breathing in a bengalese finch, looking within a single
+% breathing cycle. The two variables being compared are two pairs of
+% sequential interspike intervals. 
+%
+% Returned values:
+%   ks - this is the list of k values used
+%
+%   unreparamaterizedEstimates - this is a vector of the estimated mutual
+%   information at the values of k, for the full data set size.
+%
+%   unreparamaterizedErrorBars - this is a vector of the estimated error bars
+%   for the mutual information estimates stored in unreparamaterizedEstimates
+%
+%   reparamaterizedEstimates - this is equivalent to unreparamaterizedEstimates,
+%   but estimates were done after reparamaterizing the data into marginal
+%   standard normals.
+%
+%   reparamaterizedErrorBars - this is equivalent to 
+%   unreparamaterizedErrorBars, but for the reparamaterized data.
+%
+%-----------------------------------------------------------------------------------------
+% Copyright 2018, 2019 Caroline Holmes, Ilya Nemenman
+%-----------------------------------------------------------------------------------------
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should receive a copy of the GNU General Public License
+% along with this program.  See also <http://www.gnu.org/licenses/>.
+%-----------------------------------------------------------------------------------------
+% Please reference
+% 
+% Holmes, C.M. & Nemenman, I.  Estimation of mutual information for
+% real-valued data with error bars and controlled bias. 
+% Submitted, 2018.
+%
+% A. Kraskov, H. Stogbauer, and P. Grassberger,
+% Estimating mutual information.
+% Phys. Rev. E 69 (6) 066138, 2004
+%
+% in your published research.
 
 load('finchData.mat')
-%finchData.mat contains two variables, x and y, which 
+
 
 
 %First, let's generate some basic plots, showing the dependence of the
 %mutual information estimate on N and k
 listOfKs = [1 2 3 4 5 7 10 15 20];
 listSplitSizes = [1:10];
-findMI_KSG_bias_kN(x,y,listOfKs, listSplitSizes, 1, 0, 1);
+findMI_KSG_bias_kN(X,Y,listOfKs, listSplitSizes, 1, 0, 1);
 
 
 
 
 
-%Instead, we could make a single plot similar to figure 7 in Holmes &
+%Instead, we could make a single plot similar to figure 8 in Holmes &
 %Nemenman.
-ks = [2 5 20]; %to put it all on one plot, we need a shorter list of ks.
-%we frequently find that in higher dimensions k = 1 performs poorly and unusually, and
-%so here we'll compare using k = 2 for a low k.
+ks = [1 4 10]; %to put it all on one plot, we need a shorter list of ks.
 
 %for each k, we need a list of values of the mutual information and error
 %bars for those values.
@@ -31,13 +81,11 @@ stds = zeros(length(ks), length(listSplitSizes));
 %instead of calling findMI_KSG_bias_kN.m, we can directly call
 %findMI_KSG_subsampling.
 for i = 1:length(ks)
-    [MIs] = findMI_KSG_subsampling(x,y, ks(i), listSplitSizes,0);
+    [MIs] = findMI_KSG_subsampling(X,Y, ks(i), listSplitSizes, 0);
     for j = 1:length(listSplitSizes)
         means(i,j) = mean(MIs{j,2});
         stds(i,j) = std(MIs{j,2});
     end
-    [errorEstimate] = findMI_KSG_stddev(MIs, length(x), 1);
-    stds(i,1) = errorEstimate;
 end
 
 figure
@@ -49,32 +97,24 @@ xlabel('Number of divisions of the data')
 ylabel('Estimated Mutual Information')
 legend(horzcat('k = ', num2str(ks(1))), horzcat('k = ', num2str(ks(2))),horzcat('k = ', num2str(ks(3))))
 title('N-dependence, unreparameterized data')
+ylim([0 .5])
 
-
+unreparamaterizedEstimates = means(:,1);
+unreparamaterizedErrorBars = stds(:,1);
 
 %Reparameterize the data to compare the effects of the reparameterization.
-
-x2 = reparameterize_data(x);
-y2 = reparameterize_data(y);
-
+[X2] = reparameterize_data(X);
+[Y2] = reparameterize_data(Y);
 
 
-listOfKs = [1 2 3 4 5 7 10 15 20];
-listSplitSizes = [1:10];
-findMI_KSG_bias_kN(x2,y2,listOfKs, listSplitSizes, 1, 0, 1);
 
 %and now we can generate a similar plot, using the reparameterized data
-means = zeros(length(ks), length(listSplitSizes));
-stds = zeros(length(ks), length(listSplitSizes));
-
 for i = 1:length(ks)
-    [MIs] = findMI_KSG_subsampling(x2,y2, ks(i), listSplitSizes,0);
-    for j = 1:length(listSplitSizes);
+    [MIs] = findMI_KSG_subsampling(X2,Y2, ks(i), listSplitSizes, 0);
+    for j = 1:length(listSplitSizes)
         means(i,j) = mean(MIs{j,2});
         stds(i,j) = std(MIs{j,2});
     end
-    [errorEstimate] = findMI_KSG_stddev(MIs, length(x), 1);
-    stds(i,1) = errorEstimate;
 end
 
 figure
@@ -86,4 +126,9 @@ xlabel('Number of divisions of the data')
 ylabel('Estimated Mutual Information')
 legend(horzcat('k = ', num2str(ks(1))), horzcat('k = ', num2str(ks(2))),horzcat('k = ', num2str(ks(3))))
 title('N-dependence, reparameterized data')
+ylim([0 .5])
+
+reparamaterizedEstimates = means(:,1);
+reparamaterizedErrorBars = stds(:,1);
+
 end
